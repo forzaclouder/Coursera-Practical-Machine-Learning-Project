e7d47bc888ce225f94536e5c47bc93cb2b508fee
forzaclouder <49100366+forzaclouder@users.noreply.github.com>
2019-08-27 17:11:34 -0400

    Create code.R

---
+++ code.R
@@ -0,0 +1,32 @@
+library(caret)
+
+
+traindata<-read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv", na.strings=c("","NA", "#DIV/0!"))
+testdata<-read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv", na.strings=c("","NA", "#DIV/0!"))
+
+traindata<-traindata[,colSums(is.na(traindata))==0] 
+testdata<-testdata[,colSums(is.na(testdata))==0] 
+
+traindata<-traindata[,-c(1:7)]
+testdata<-testdata[,-c(1:7)]
+
+set.seed(2019) 
+inTrain<-createDataPartition(traindata$classe,p=0.7,list=F)
+trainingset<-traindata[inTrain,]
+validationset<-traindata[-inTrain,]
+
+control<-trainControl(method="cv", 5)
+model<-train(classe~.,data=trainingset,method="rf",trControl=control,ntree=250)
+vi <- varImp(model)$importance
+
+predict<-predict(modelRf,validationset)
+
+accuracy<-postResample(predict,validationset$classe)
+
+result<-predict(model,testdata[,-length(names(testdata))])
+
+model
+confusionMatrix(validationset$classe,predict)
+vi[head(order(unlist(vi), decreasing = TRUE), 5L), , drop = FALSE]
+accuracy
+result
